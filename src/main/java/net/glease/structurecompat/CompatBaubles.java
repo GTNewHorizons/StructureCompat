8f494fde8398e5853a0059206a87ab90ee6f177c
GitHub GTNH Actions <>
2024-04-15 12:34:06 +0000

    spotlessApply

--- src/main/java/net/glease/structurecompat/CompatBaubles.java
+++ src/main/java/net/glease/structurecompat/CompatBaubles.java
@@ -1,25 +1,30 @@
 package net.glease.structurecompat;
 
-import baubles.api.BaublesApi;
-import com.gtnewhorizon.structurelib.util.InventoryIterable;
-import com.gtnewhorizon.structurelib.util.InventoryUtility;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.inventory.IInventory;
 
+import com.gtnewhorizon.structurelib.util.InventoryIterable;
+import com.gtnewhorizon.structurelib.util.InventoryUtility;
+
+import baubles.api.BaublesApi;
+
 @Compat("Baubles")
 public class CompatBaubles {
+
     public CompatBaubles() {
-        InventoryUtility.registerInventoryProvider("5001-baubles", new InventoryUtility.InventoryProvider<InventoryIterable<IInventory>>() {
+        InventoryUtility.registerInventoryProvider(
+                "5001-baubles",
+                new InventoryUtility.InventoryProvider<InventoryIterable<IInventory>>() {
 
-            @Override
-            public InventoryIterable<IInventory> getInventory(EntityPlayerMP player) {
-                return new InventoryIterable<>(BaublesApi.getBaubles(player));
-            }
+                    @Override
+                    public InventoryIterable<IInventory> getInventory(EntityPlayerMP player) {
+                        return new InventoryIterable<>(BaublesApi.getBaubles(player));
+                    }
 
-            @Override
-            public void markDirty(InventoryIterable<IInventory> inv) {
-                inv.getInventory().markDirty();
-            }
-        });
+                    @Override
+                    public void markDirty(InventoryIterable<IInventory> inv) {
+                        inv.getInventory().markDirty();
+                    }
+                });
     }
 }
